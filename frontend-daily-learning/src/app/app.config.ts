983b9ac3ce8c97de7805c0af91c325486ec17755
coolrajesh <rajeshroy8686@gmail.com>
2026-06-21 14:43:29 +0530

    config changes

--- frontend-daily-learning/src/app/app.config.ts
+++ frontend-daily-learning/src/app/app.config.ts
@@ -2,7 +2,6 @@ import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
-import { MarkdownModule } from 'ngx-markdown';
 import { importProvidersFrom } from '@angular/core';
 import { provideMarkdown } from 'ngx-markdown';
 import { HttpClientModule } from '@angular/common/http';
@@ -12,8 +11,6 @@ export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
     importProvidersFrom(HttpClientModule),
-    provideMarkdown(),  
-    importProvidersFrom(MarkdownModule.forRoot()),
-
+    provideMarkdown(),
   ]
 };
